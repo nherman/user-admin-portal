cd514a6c1fa302f2317b17c2deb66542b8bf4ff2
nick <nherman@gmail.com>
2026-07-03 22:50:32 -0400

    adding springboot template

---
+++ backend/src/main/java/com/nherman/useradminportal/UserAdminPortalApplication.java
@@ -0,0 +1,13 @@
+package com.nherman.useradminportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAdminPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAdminPortalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nherman/useradminportal/UserAdminPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nherman.useradminportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAdminPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
